7b2c850050b01d4bf16300f1fdcdf262f24a3bcd
Tyler Howard <tylerhoward@trailblazer.towson.edu>
2017-10-24 18:00:51 -0400

    add loading animation, start of local storage for api data, switching hosting to heroku

--- src/app/load-data.service.ts
+++ src/app/load-data.service.ts
@@ -5,13 +5,24 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class LoadDataService {
   jsonHeader: Headers;
+  dataIn: any;
 
   constructor(public http: Http) {
     this.jsonHeader = new Headers();
     this.jsonHeader.append('Content-Type', 'application/json');
   }
   getData() {
-    return this.http.get('/api/data', { headers: this.jsonHeader }).map((res: Response) => res.json());
     //return this.http.get('assets/phishTank.json').map((res: Response) => res.json());
+    if (localStorage['data']) {
+      this.dataIn = JSON.parse(localStorage.getItem('data'));
+      console.log(this.dataIn);
+      console.log('In local storage');
+    } else {
+      this.dataIn = this.http.get('/api/data', { headers: this.jsonHeader }).map((res: Response) => res.json());
+      localStorage.setItem('data', JSON.stringify(this.dataIn));
+      console.log(this.dataIn);
+      console.log('Calling API');
+    }
+    return this.dataIn;
   }
 }

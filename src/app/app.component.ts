56e2a0c171cdbdd8ea396c63f6ac34c4fa922eb7
Tyler Howard <tylerhoward@trailblazer.towson.edu>
2017-10-25 19:09:34 -0400

    added sorting by date and month

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,25 +4,28 @@ import { LocationService } from './location.service';
 import { LoadDataService } from './load-data.service';
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  model;
   title = 'Current Active Phishing Threats';
   phishData: any;
   phishLoc: Array<Object>;
   targets: Array<String>;
   countries: Array<String>;
-  dates: Array<String>;
+  months: Array<Object>;
+  years: Array<String>;
   targetCount: any;
   countryCount: any;
   dateCount: any;
   sortedData: any;
   countrySelect: String;
   targetSelect: String;
+  monthSelect: Object;
+  yearSelect: String;
   noResults: Boolean;
   canSearch: Boolean;
   countingDone: Boolean;
@@ -99,6 +102,8 @@ export class AppComponent implements OnInit {
     this.phishData = this.dataHold;
     this.countrySelect = '';
     this.targetSelect = '';
+    this.monthSelect = '';
+    this.yearSelect = '';
   }
 
   selectCountry(country: string) {
@@ -108,28 +113,51 @@ export class AppComponent implements OnInit {
   selectTarget(target: string) {
     this.targetSelect = target;
   }
+  selectMonth(month: Object) {
+    this.monthSelect = month;
+  }
+  selectYear(year: string) {
+    this.yearSelect = year;
+  }
 
   filterData() {
     const t = this.targetSelect;
     const c = this.countrySelect;
+    const m = this.monthSelect['n'];
+
+    const y = this.yearSelect;
+
     this.canSearch = false;
-    if (t === '' && c === '') {
+    if (t === '' && c === '' && m === '' && y === '') {
       this.canSearch = true;
     }
     this.noResults = false;
     this.sortedData = [];
     this.sortedData = this.phishData.filter(function (item) {
-      if (t !== '' && c !== '') {
-         return (item.target === t) && (item.country === c);
+      if (t && c ) {
+        return (item.target === t) && (item.country === c);
       } else
-      if (t !== '' && c === '') {
+      if (t && !c) {
         return (item.target === t);
       } else
-      if (t === '' && c !== '') {
+      if (!t && c) {
         return (item.country === c);
       } else {
-        this.noResults = true;
-        return (item);
+        return item;
+      }
+    });
+    this.sortedData = this.sortedData.filter(function (item) {
+      const d = new Date(item.verification_time);
+      if (m && y) {
+        return (d.getMonth() === m) && (d.getFullYear().toString() === y);
+      } else
+      if (m && !y) {
+         return (d.getMonth() === m);
+      } else
+      if (!m && y) {
+         return (d.getFullYear().toString() === y);
+      } else {
+         return item;
       }
     });
     if (this.sortedData.length === 0) {
@@ -142,8 +170,11 @@ export class AppComponent implements OnInit {
   initSorter() {
     this.targets = [];
     this.countries = [];
-    this.dates = [];
-
+    this.years = ['2015', '2016', '2017'];
+    this.months = [{ 's': 'January', 'n': 0} , { 's': 'February', 'n': 1} , { 's': 'March', 'n': 2} ,
+                    { 's': 'April', 'n': 3} , { 's': 'May', 'n': 4} , { 's': 'June', 'n': 5} ,
+                     { 's': 'July', 'n': 6} , { 's': 'August', 'n': 7} , { 's': 'September', 'n': 8},
+                      { 's': 'October', 'n': 9} , { 's': 'November', 'n': 10} , { 's': 'December', 'n': 11} ];
     this.targetCount = this.counter.countTarget(this.phishData);
     for (let i = 0; i < this.targetCount.length; i++) {
       this.targets[i] = this.targetCount[i]['id'];
@@ -156,9 +187,15 @@ export class AppComponent implements OnInit {
       this.countries[i] = this.countryCount[i]['id'];
     }
     this.countries.sort().splice(0, 1); // Will be "no country"
-
+    this.sortByDate();
     this.dateCount = this.counter.countDates(this.phishData);
     this.countingDone = true;
+    }
+
+    sortByDate() {
+      this.phishData.sort(function (a, b) {
+        return +new Date(b.verification_time) - +new Date(a.verification_time);
+      });
     }
 
 }
